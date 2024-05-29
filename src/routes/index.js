d25787ba9ae6315fd76ef1c853acdc4225aadfdb
adidaher <adedaher5@gmail.com>
2024-05-29 11:32:47 +0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,169 @@
+import React from "react"
+import { Navigate } from "react-router-dom"
+
+// Pages Component
+import Chat from "../pages/Chat/Chat"
+
+// File Manager
+import FileManager from "../pages/FileManager/index"
+
+// Profile
+import UserProfile from "../pages/Authentication/user-profile"
+
+// Pages Calendar
+import Calendar from "../pages/Calendar/index"
+
+// //Tasks
+import TasksList from "../pages/Tasks/tasks-list"
+import TasksCreate from "../pages/Tasks/tasks-create"
+import TasksKanban from "../pages/Tasks/tasks-kanban"
+
+// //Projects
+import ProjectsGrid from "../pages/Projects/projects-grid"
+import ProjectsList from "../pages/Projects/projects-list"
+import ProjectsOverview from "../pages/Projects/ProjectOverview/projects-overview"
+import ProjectsCreate from "../pages/Projects/projects-create"
+
+//Email
+import EmailInbox from "../pages/Email/email-inbox"
+import EmailRead from "../pages/Email/email-read"
+import EmailBasicTemplte from "../pages/Email/email-basic-templte"
+import EmailAlertTemplte from "../pages/Email/email-template-alert"
+import EmailTemplateBilling from "../pages/Email/email-template-billing"
+
+// Authentication related pages
+import Login from "../pages/Authentication/Login"
+import Logout from "../pages/Authentication/Logout"
+import Register from "../pages/Authentication/Register"
+import ForgetPwd from "../pages/Authentication/ForgetPassword"
+
+//  // Inner Authentication
+import Login1 from "../pages/AuthenticationInner/Login"
+import Login2 from "../pages/AuthenticationInner/Login2"
+import Register1 from "../pages/AuthenticationInner/Register"
+import Register2 from "../pages/AuthenticationInner/Register2"
+import Recoverpw from "../pages/AuthenticationInner/Recoverpw"
+import Recoverpw2 from "../pages/AuthenticationInner/Recoverpw2"
+import ForgetPwd1 from "../pages/AuthenticationInner/ForgetPassword"
+import ForgetPwd2 from "../pages/AuthenticationInner/ForgetPassword2"
+import LockScreen from "../pages/AuthenticationInner/auth-lock-screen"
+import LockScreen2 from "../pages/AuthenticationInner/auth-lock-screen-2"
+import ConfirmMail from "../pages/AuthenticationInner/page-confirm-mail"
+import ConfirmMail2 from "../pages/AuthenticationInner/page-confirm-mail-2"
+import EmailVerification from "../pages/AuthenticationInner/auth-email-verification"
+import EmailVerification2 from "../pages/AuthenticationInner/auth-email-verification-2"
+import TwostepVerification from "../pages/AuthenticationInner/auth-two-step-verification"
+import TwostepVerification2 from "../pages/AuthenticationInner/auth-two-step-verification-2"
+
+// Dashboard
+import Dashboard from "../pages/Dashboard/index"
+import DashboardSaas from "../pages/Dashboard-saas/index"
+//import DashboardCrypto from "../pages/Dashboard-crypto/index";
+import Blog from "../pages/Dashboard-Blog/index"
+
+//Pages
+import PagesStarter from "../pages/Utility/pages-starter"
+import PagesMaintenance from "../pages/Utility/pages-maintenance"
+import PagesComingsoon from "../pages/Utility/pages-comingsoon"
+import PagesTimeline from "../pages/Utility/pages-timeline"
+import PagesFaqs from "../pages/Utility/pages-faqs"
+import PagesPricing from "../pages/Utility/pages-pricing"
+import Pages404 from "../pages/Utility/pages-404"
+import Pages500 from "../pages/Utility/pages-500"
+
+//Contacts
+import ContactsGrid from "../pages/Contacts/contacts-grid"
+import ContactsList from "../pages/Contacts/ContactList/contacts-list"
+import ContactsProfile from "../pages/Contacts/ContactsProfile/contacts-profile"
+
+const authProtectedRoutes = [
+  { path: "/dashboard", component: <Dashboard /> },
+  { path: "/dashboard-saas", component: <DashboardSaas /> },
+
+  { path: "/blog", component: <Blog /> },
+
+  //chat
+  { path: "/chat", component: <Chat /> },
+
+  //File Manager
+  { path: "/apps-filemanager", component: <FileManager /> },
+
+  // //calendar
+  { path: "/calendar", component: <Calendar /> },
+
+  // //profile
+  { path: "/profile", component: <UserProfile /> },
+
+  //Email
+  { path: "/email-inbox", component: <EmailInbox /> },
+  { path: "/email-read/:id?", component: <EmailRead /> },
+  { path: "/email-template-basic", component: <EmailBasicTemplte /> },
+  { path: "/email-template-alert", component: <EmailAlertTemplte /> },
+  { path: "/email-template-billing", component: <EmailTemplateBilling /> },
+
+  // Tasks
+  { path: "/tasks-list", component: <TasksList /> },
+  { path: "/tasks-create", component: <TasksCreate /> },
+  { path: "/tasks-kanban", component: <TasksKanban /> },
+
+  //Projects
+  { path: "/projects-grid", component: <ProjectsGrid /> },
+  { path: "/projects-list", component: <ProjectsList /> },
+  { path: "/projects-overview", component: <ProjectsOverview /> },
+  { path: "/projects-overview/:id", component: <ProjectsOverview /> },
+  { path: "/projects-create", component: <ProjectsCreate /> },
+
+  // Contacts
+  { path: "/contacts-grid", component: <ContactsGrid /> },
+  { path: "/contacts-list", component: <ContactsList /> },
+  { path: "/contacts-profile", component: <ContactsProfile /> },
+
+  //Utility
+  { path: "/pages-starter", component: <PagesStarter /> },
+  { path: "/pages-timeline", component: <PagesTimeline /> },
+  { path: "/pages-faqs", component: <PagesFaqs /> },
+  { path: "/pages-pricing", component: <PagesPricing /> },
+
+  // this route should be at the end of all other routes
+  // eslint-disable-next-line react/display-name
+  {
+    path: "/",
+    exact: true,
+    component: <Navigate to="/dashboard" />,
+  },
+]
+
+const publicRoutes = [
+  { path: "/login", component: <Login /> },
+  { path: "/logout", component: <Logout /> },
+  { path: "/forgot-password", component: <ForgetPwd /> },
+  { path: "/register", component: <Register /> },
+
+  { path: "/pages-maintenance", component: <PagesMaintenance /> },
+  { path: "/pages-comingsoon", component: <PagesComingsoon /> },
+  { path: "/pages-404", component: <Pages404 /> },
+  { path: "/pages-500", component: <Pages500 /> },
+
+  // Authentication Inner
+  { path: "/pages-login", component: <Login1 /> },
+  { path: "/pages-login-2", component: <Login2 /> },
+  { path: "/pages-register", component: <Register1 /> },
+  { path: "/pages-register-2", component: <Register2 /> },
+  { path: "/page-recoverpw", component: <Recoverpw /> },
+  { path: "/page-recoverpw-2", component: <Recoverpw2 /> },
+  { path: "/pages-forgot-pwd", component: <ForgetPwd1 /> },
+  { path: "/auth-forgot-pwd-2", component: <ForgetPwd2 /> },
+  { path: "/auth-lock-screen", component: <LockScreen /> },
+  { path: "/auth-lock-screen-2", component: <LockScreen2 /> },
+  { path: "/page-confirm-mail", component: <ConfirmMail /> },
+  { path: "/page-confirm-mail-2", component: <ConfirmMail2 /> },
+  { path: "/auth-email-verification", component: <EmailVerification /> },
+  { path: "/auth-email-verification-2", component: <EmailVerification2 /> },
+  { path: "/auth-two-step-verification", component: <TwostepVerification /> },
+  {
+    path: "/auth-two-step-verification-2",
+    component: <TwostepVerification2 />,
+  },
+]
+
+export { authProtectedRoutes, publicRoutes }
